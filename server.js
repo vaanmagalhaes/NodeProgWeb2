09a40cb5d065e9f39bd7c7375090eb1f2e05aa9c
Vanessa Magalhães <111777241+vaanmagalhaes@users.noreply.github.com>
2026-03-17 19:02:25 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import app from "./src/app.js";
-
+import conexao from "./src/app/database/index.js";
 const port = 3000;
 
+//realizando a conexão
+//conexao.connect((erro) => {
+ //       if(erro) {
+//        console.log("Erro na conexão: ", erro)
+  //  } else {
+    //    console.log("Conectado com sucesso!")
+    //}
+//});
+
+
+//escutando a porta
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
